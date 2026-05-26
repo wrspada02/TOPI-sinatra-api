809eecf3149849154552afbdd493a7977d898081
William Spada <90157791+wrspada02@users.noreply.github.com>
2026-05-26 23:04:11 +0000

    chore: validate object type

--- app.rb
+++ app.rb
@@ -7,7 +7,12 @@ redis = Redis.new(host: '', port: 6379)
 post '/enqueue/:item' do
     item_value = params[:item]
 
+    if (!item_value.is_a?(Numeric)) {
+        status 400
+        { message: 'Item must be numeric' }.to_json
+    }
+
     redis.lpush('items', item)
     status 200
-    { message: 'Enqueued item #{item_value} to redis queue' }.to_json 
+    { message: 'Enqueued item #{item_value} to redis queue' }.to_json
 end
